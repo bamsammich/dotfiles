63e9c12817d2a4736ea7ce1f7f15cdfb9c18783d
Travis Huddleston <travis.huddleston@immuta.com>
2024-07-24 14:52:29 -0400

    Add scratch/scripts/Remove-GitMergedBranches.ps1

---
+++ scratch/scripts/executable_Remove-GitMergedBranches.ps1
@@ -0,0 +1,10 @@
+#! /usr/bin/env pwsh
+git fetch -p
+
+$branches = git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | `
+  Where-Object { $_ -match "refs/heads/(.*)\s?\[gone\]" } | `
+  ForEach-Object { $matches[1].Trim() }
+
+foreach ($branch in $branches) {
+  git branch -D $branch
+}
