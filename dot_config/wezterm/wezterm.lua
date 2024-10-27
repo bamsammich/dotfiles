604ded71398cddace29c6cc37b54c141c044ff82
Travis Huddleston <travis.huddleston@immuta.com>
2024-10-27 15:47:56 -0400

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,6 +9,20 @@ config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font 'CaskaydiaMono Nerd Font'
 config.font_size = 13.0
 
+-- keys
+config.keys = {
+  {
+    key = 'h',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = "l",
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+}
+
 -- tabs
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config)
