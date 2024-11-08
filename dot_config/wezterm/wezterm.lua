02276ae7542642f11986126d8c131a47a9d4024a
Travis Huddleston <travis.huddleston@immuta.com>
2024-11-08 09:21:04 -0500

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,11 +1,27 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+local get_appearance = function()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
+end
+
+local scheme_for_appearance = function(appearance)
+  if appearance:find("Dark") then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.native_macos_fullscreen_mode = true
 
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.font = wezterm.font 'CaskaydiaMono Nerd Font'
 config.font_size = 13.0
 
@@ -31,6 +47,11 @@ config.keys = {
     mods = 'CMD|SHIFT',
     action = wezterm.action.ActivatePaneDirection 'Up',
   },
+  {
+    key = 'f',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ToggleFullScreen,
+  }
 }
 
 -- tabs
