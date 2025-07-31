ac556c62915f6a9e86d3758957df6c38f9ac52cc
Travis Huddleston <thuddleston@kududyn.com>
2025-07-31 10:06:43 -0400

    Add .config/ghostty/config Update ghostty_shaders.sh

--- run_once_ghostty_shaders.sh
+++ run_once_ghostty_shaders.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-dir="${HOME}/.config/ghostty/config/shaders"
+dir="${HOME}/.config/ghostty/shaders"
 
 mkdir -p "${dir}"
 git clone https://github.com/hackr-sh/ghostty-shaders "${dir}"
