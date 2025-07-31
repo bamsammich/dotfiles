758892d471a53ce49521b52a2e7364307ef32a7e
Travis Huddleston <thuddleston@kududyn.com>
2025-07-31 10:30:46 -0400

    Update .config/ghostty/config Update ghostty_shaders.sh

--- run_once_ghostty_shaders.sh
+++ run_once_ghostty_shaders.sh
@@ -2,5 +2,8 @@
 
 dir="${HOME}/.config/ghostty/shaders"
 
-mkdir -p "${dir}"
-git clone https://github.com/hackr-sh/ghostty-shaders "${dir}"
+mkdir -p "${dir}/hackr-sh/ghostty-shaders"
+git clone https://github.com/hackr-sh/ghostty-shaders "${dir}/hackr-sh/ghostty-shaders"
+
+mkdir -p "${dir}/KroneCorylus/ghostty-shader-playground"
+git clone https://github.com/KroneCorylus/ghostty-shader-playground "${dir}/KroneCorylus/ghostty-shader-playground"
