5cd98be730fdf3426ff17d9f8a589cd91d99239d
Travis Huddleston <thuddleston@kududyn.com>
2025-09-23 13:58:55 -0400

    Update .chezmoidata.yaml Update .config/ghostty/config Update ghostty_themes.sh Add intall-tpm.sh

--- run_onchange_ghostty_themes.sh
+++ run_onchange_ghostty_themes.sh
@@ -10,4 +10,8 @@ mkdir -p "${tmp_dir}/EastSun5566/ghostty-noctis-themes"
 git clone https://github.com/eastsun5566/ghostty-noctis-themes.git "${tmp_dir}/EastSun5566/ghostty-noctis-themes"
 cp $tmp_dir/EastSun5566/ghostty-noctis-themes/themes/* $theme_dir/
 
+mkdir -p "${tmp_dir}/catppuccin/ghostty"
+git clone https://github.com/catppuccin/ghostty.git "${tmp_dir}/catppuccin/ghostty"
+cp $tmp_dir/catppuccin/ghostty/* $theme_dir/
+
 rm -rf $tmp_dir
